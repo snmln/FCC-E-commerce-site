5878233e3fcb55b77586ff75c903adb664cf312b
config/allWaffle <33710544+enolamsean@users.noreply.github.com>
2020-03-13 18:09:41 -0400

    Product page linking to individual product details
    Functionality of the product page linking to specific product detail pages.

--- src/Context.js
+++ src/Context.js
@@ -26,9 +26,21 @@ componentDidMount() {
     this.setState(()=>{
         return{products:tempProducts};
     });
+};
+
+getItem = (id) =>{
+    const product = this.state.products.find(item  => item.id ===id);
+    return product;
 }
-handleDetail = () =>{console.log('hello from detail')};
-    addToCart = (id) =>{console.log(`hello from add to cart.id is ${id}`)};
+
+handleDetail = (id) =>{
+    const product = this.getItem(id);
+    this.setState(()=>{
+        return{detailProduct:product};
+    })
+    };
+    addToCart = (id) =>{
+        console.log(`hello from add to cart.id is ${id}`)};
 
     render() {
         return (
