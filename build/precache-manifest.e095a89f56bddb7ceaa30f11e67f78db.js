cbcfb0072899f6fdc00b8740b9747fd77b17a1d1
Sean Malone <33710544+snmln@users.noreply.github.com>
2020-08-13 18:26:27 -0400

    Adding Build for Netlify deployment

---
+++ build/precache-manifest.e095a89f56bddb7ceaa30f11e67f78db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e35ed26b25e8ab0811fdf89e00d4e3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d92148cc5488d5fa3d",
+    "url": "/static/css/2.1f9835b2.chunk.css"
+  },
+  {
+    "revision": "060cce7b1ca72d0f912b",
+    "url": "/static/css/main.080a3715.chunk.css"
+  },
+  {
+    "revision": "88d92148cc5488d5fa3d",
+    "url": "/static/js/2.32f35ae0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.32f35ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060cce7b1ca72d0f912b",
+    "url": "/static/js/main.8e7ecf68.chunk.js"
+  },
+  {
+    "revision": "fb3117157f9734dc5602",
+    "url": "/static/js/runtime-main.4cce29b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
